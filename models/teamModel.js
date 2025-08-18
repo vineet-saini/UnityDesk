0c9aaa663c5c565e360a155a8c0ec18d13dcd5ac
Uday <uday2692004@gmail.com>
2025-08-18 10:52:02 +0530

    Updated server.js and added team model, controller, and routes

---
+++ models/teamModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose;
+
+const TeamSchema = new Schema({
+    teamName:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    members:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    projects:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Project"
+    }],
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model("Team",TeamSchema);
