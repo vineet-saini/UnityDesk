485db47bc77f4ffcf1edff69da7cfa49bb790c5f
vineet-saini <123089775+vineet-saini@users.noreply.github.com>
2025-10-16 11:19:10 +0530

    Delete backend2 directory

--- backend2/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { adminDashboard } = require("../controllers/adminController");
-const { verifyToken, authorizeRoles } = require("../middleware/authMiddleware");
-
-// Only admins can access
-router.get("/dashboard", verifyToken, authorizeRoles("admin"), adminDashboard);
-
-module.exports = router;
